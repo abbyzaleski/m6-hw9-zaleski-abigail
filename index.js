6bdb7206b15c1bd71de9fef2951a7dc5982bfce3
Abby Zaleski <azaleski1@ufl.edu>
2021-10-31 20:22:34 -0400

    continued updating js code

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var btn = document.querySelector('button')
 var forecastE1 = document.getElementById('forecast')
-var inputValue = document.querySelector('.inputValue')
+var inputValue = document.querySelector('.weather-search')
 var name = document.querySelector('.name');
 var descriptor = document.querySelector('.descriptor');
 var temp = document.querySelector('.temp');
 
 btn.onclick = function() {
-    console.log('clicked')
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=82133d0b69b88c6cf71f71c9df46bd5f")
+    // console.log('clicked')
+    fetch("https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&APPID=82133d0b69b88c6cf71f71c9df46bd5f")
     .then(function(res) {
         return res.json()
     })
